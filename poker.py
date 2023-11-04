1e1906dd746d535a3d1ec1e40cdba668720fb28b
samfeldman824 <samuel_feldman@brown.edu>
2023-11-04 18:56:20 -0400

    updated functions for adding and printing game

--- poker.py
+++ poker.py
@@ -47,7 +47,7 @@ import os
 #     json.dump(data, json_file, indent=4)
 
 
-def add_poker_game(game_data_path: str, dest_path: str):
+def add_poker_game(game_data_path: str, dest_path: str, exclude_list=[]):
     if not game_data_path.endswith(".csv"):
         print("Error: Game data file must be a CSV File")
         sys.exit(1)
@@ -76,15 +76,32 @@ def add_poker_game(game_data_path: str, dest_path: str):
 
     net_winnings_by_player = (game_data.groupby('player_nickname')['net'].sum() / 100).to_dict()
     
+    net_winnings_by_player = {key: value for key, value in net_winnings_by_player.items() if key not in exclude_list}
+
+
+    players_updated = 0
+    players_updated_list = []
+
     for player in json_data:
         for name in player['player_nicknames']:
             if name in net_winnings_by_player:
-                print(name, net_winnings_by_player[name])
+                # print(name, net_winnings_by_player[name])
                 player['net'] += net_winnings_by_player[name]
                 player['games_played'].append(day)
+                players_updated += 1
+                players_updated_list.append(name)
 
-    with open(dest_path, 'w') as json_file:
-        json.dump(json_data, json_file, indent=4)
+    if players_updated == len(net_winnings_by_player):
+        for name, net in net_winnings_by_player.items():
+            print(name, net)
+        with open(dest_path, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4)
+        sort_days_list(dest_path)
+    else:
+        for name in net_winnings_by_player.keys():
+            if name not in players_updated_list:
+                print(f"{name}")
+        print('Not all players known')
 
 def add_all_games(folder_path: str, dest_path: str):
     for file in os.listdir(folder_path):
@@ -92,6 +109,23 @@ def add_all_games(folder_path: str, dest_path: str):
             filepath = 'ledgers/' + file
             add_poker_game(filepath, dest_path)
 
+def print_winnings_of_game(ledger_path: str):
+    if not ledger_path.endswith(".csv"):
+        print("Error: Game ledger file must be a CSV File")
+        sys.exit(1)
+
+    try:
+        game_data = pd.read_csv(ledger_path)
+        
+    except FileNotFoundError as e:
+        print('File not found', e)
+        sys.exit(1)
+
+    net_winnings_by_player = (game_data.groupby('player_nickname')['net'].sum() / 100).to_dict()
+    sorted_winnings = dict(sorted(net_winnings_by_player.items(), key=lambda item: item[1], reverse=True))
+    for name, net in sorted_winnings.items():
+        print(f'{name}: {net}')
+    
 def unique_nicknames(file_path: str):
     unique_nicknames = set()
     
@@ -137,9 +171,10 @@ def sort_days_list(json_path: str):
 
 if __name__ == "__main__":
     
-    # add_poker_game('ledgers/ledger10_30.csv', 'data.json')
+    add_poker_game('ledgers/ledger10_31(2).csv', 'data.json', ['grant lumkong'])
     # unique_nicknames('ledgers')
     # reset_net_games_played('data.json')
-    sort_days_list('data.json')
+    # sort_days_list('data.json')
     # add_all_games('ledgers', 'data.json')
-    print('done')
+    # print_winnings_of_game('ledgers/ledger10_31(2).csv')
+    print('\ndone')
