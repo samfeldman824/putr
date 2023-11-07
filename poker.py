bcc5c59e7c9ba619fbc88242d55e9ac2b6551331
samfeldman824 <samuel_feldman@brown.edu>
2023-11-07 18:04:17 -0500

    updated backend functions

--- poker.py
+++ poker.py
@@ -4,7 +4,6 @@ import sys
 import re
 import os
 
-
 def add_poker_game(game_data_path: str, dest_path: str, exclude_list=[]):
     if not game_data_path.endswith(".csv"):
         print("Error: Game data file must be a CSV File")
@@ -49,8 +48,13 @@ def add_poker_game(game_data_path: str, dest_path: str, exclude_list=[]):
                 # print(name, net_winnings_by_player[name])
                 player["net"] += net_winnings_by_player[name]
                 player["games_played"].append(day)
+                player["biggest_win"] = max(player["biggest_win"], net_winnings_by_player[name])
+                player["biggest_loss"] = min(player["biggest_loss"], net_winnings_by_player[name])
+                player["highest_net"] = max(player["highest_net"], player["net"])
+                player["lowest_net"] = min(player["lowest_net"], player["net"])
                 players_updated += 1
                 players_updated_list.append(name)
+                
 
     if players_updated == len(net_winnings_by_player):
         for name, net in net_winnings_by_player.items():
@@ -65,11 +69,12 @@ def add_poker_game(game_data_path: str, dest_path: str, exclude_list=[]):
         print("Not all players known")
 
 
-def add_all_games(folder_path: str, dest_path: str):
-    for file in os.listdir(folder_path):
+def add_all_games(folder_path: str, dest_path: str, exclude_list=[]):
+    for file in sorted(os.listdir(folder_path)):
         if file.endswith(".csv"):
             filepath = "ledgers/" + file
-            add_poker_game(filepath, dest_path)
+            print(filepath)
+            add_poker_game(filepath, dest_path, exclude_list)
 
 
 def print_winnings_of_game(ledger_path: str):
@@ -119,6 +124,10 @@ def reset_net_games_played(json_path: str):
     for player in json_data:
         player["net"] = 0
         player["games_played"] = []
+        player["biggest_win"] = 0
+        player["biggest_loss"] = 0
+        player["highest_net"] = 0
+        player["lowest_net"] = 0
 
     with open(json_path, "w") as json_file:
         json.dump(json_data, json_file, indent=4)
@@ -140,11 +149,29 @@ def sort_days_list(json_path: str):
         json.dump(json_data, json_file, indent=4)
 
 
+def add_fields(json_path: str):
+    try:
+        with open(json_path, "r") as dest_file:
+            json_data = json.load(dest_file)
+
+    except FileNotFoundError as e:
+        print("Error reading the data file:", e)
+        sys.exit(1)
+
+    for player in json_data:
+        player["highest_net"] = 0
+        player["lowest_net"] = 0
+
+    with open(json_path, "w") as json_file:
+        json.dump(json_data, json_file, indent=4)
+
+
 if __name__ == "__main__":
-    # add_poker_game("ledgers/ledger11_6(1).csv", "data.json", [])
+    # add_poker_game("ledgers/ledger11_7.csv", "data.json", [])
     # unique_nicknames('ledgers')
     # reset_net_games_played('data.json')
     # sort_days_list('data.json')
-    # add_all_games('ledgers', 'data.json')
+    # add_all_games('ledgers', 'data.json', ["Ethan", "Theo", "Father Kasarov", "lukas", "tiff", "grant lumkong"])
     # print_winnings_of_game("ledgers/ledger11_6.csv")
+    # add_fields("data.json")
     print("\ndone")
