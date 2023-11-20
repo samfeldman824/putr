7a9da52f6f53d87c2a056eac22a356576f5a433c
samfeldman824 <samuel_feldman@brown.edu>
2023-11-20 14:43:52 -0500

    fixture cleanup

--- test_poker.py
+++ test_poker.py
@@ -19,25 +19,12 @@ def temp_dir_fixture():
         new_ledger_path = os.path.join(tempdir, os.path.basename(original_ledger_path))
         shutil.copytree(original_ledger_path, new_ledger_path)
 
-
-        # Print the contents of the temp directory for verification
-        # print(f"Contents of the temporary directory {tempdir}:")
-        # for root, dirs, files in os.walk(tempdir):
-        #     for name in files:
-        #         print(os.path.join(root, name))
-        #     for name in dirs:
-        #         print(os.path.join(root, name))
-
-        # poker = Poker(new_ledger_path, new_json_path + "/mock1_data.json")
-
        # Create the Poker instance
         json_path = os.path.join(new_json_path, "mock1_data.json")
         poker = Poker(new_ledger_path, json_path)
 
         # Yield both the poker instance and the paths
         yield poker, new_ledger_path, json_path
-
-        # yield tempdir
         
 
 def test_add_poker_game(temp_dir_fixture, capfd):
