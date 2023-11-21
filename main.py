c2b481419805394f485fd7ec10c03a6d75633ada
samfeldman824 <samuel_feldman@brown.edu>
2023-11-20 19:37:00 -0500

    moved execution commands to main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from poker import Poker
+import sys
+
+def main():
+    # add_poker_game("ledgers/ledger11_09.csv", "data.json", [])
+    # unique_nicknames('ledgers')
+    # reset_net_games_played('data.json')
+    # sort_days_list('data.json')
+    # add_all_games('ledgers', 'data.json', ["Ethan", "Theo", "Father Kasarov", "lukas", "tiff", "grant lumkong"])
+    # print_winnings_of_game("ledgers/ledger11_07(1).csv")
+    # add_fields("data.json")
+    poker = Poker("ledgers", "data.json")
+    # poker.add_poker_game("fake_path.csv")
+    # poker.reset_net_fields()
+    # poker.add_all_games(["Ethan", "Theo", "Father Kasarov", "lukas", "tiff", "grant lumkong"])
+    # poker.add_poker_game("ledgers/ledger11_10.csv")
+    # poker.add_field()
+
+
+    # poker.reset_net_fields()
+    if len(sys.argv) > 1:
+
+        # for adding game
+        if sys.argv[1] == "ag":
+            csv_path = f"{poker.ledger_folder_path}/ledger{sys.argv[2]}.csv"
+            poker.add_poker_game(csv_path)
+            
+        # for printing results of game
+        if sys.argv[1] == "pg":
+            csv_path = f"{poker.ledger_folder_path}/ledger{sys.argv[2]}.csv"
+            poker.print_game_results(csv_path)
+
+        if sys.argv[1] == "pgs":
+            poker.print_all_games()
+
+
+    
+    print("\ndone")
+
+if __name__ == "__main__":
+    main()
--- poker.py
+++ poker.py
@@ -5,42 +5,6 @@ import re
 import os
 from poker_utils import get_extreme_names
 
-def main():
-    # add_poker_game("ledgers/ledger11_09.csv", "data.json", [])
-    # unique_nicknames('ledgers')
-    # reset_net_games_played('data.json')
-    # sort_days_list('data.json')
-    # add_all_games('ledgers', 'data.json', ["Ethan", "Theo", "Father Kasarov", "lukas", "tiff", "grant lumkong"])
-    # print_winnings_of_game("ledgers/ledger11_07(1).csv")
-    # add_fields("data.json")
-    poker = Poker("ledgers", "data.json")
-    # poker.add_poker_game("fake_path.csv")
-    # poker.reset_net_fields()
-    # poker.add_all_games(["Ethan", "Theo", "Father Kasarov", "lukas", "tiff", "grant lumkong"])
-    # poker.add_poker_game("ledgers/ledger11_10.csv")
-    # poker.add_field()
-
-
-    # poker.reset_net_fields()
-    if len(sys.argv) > 1:
-
-        # for adding game
-        if sys.argv[1] == "ag":
-            csv_path = f"{poker.ledger_folder_path}/ledger{sys.argv[2]}.csv"
-            poker.add_poker_game(csv_path)
-            
-        # for printing results of game
-        if sys.argv[1] == "pg":
-            csv_path = f"{poker.ledger_folder_path}/ledger{sys.argv[2]}.csv"
-            poker.print_game_results(csv_path)
-
-        if sys.argv[1] == "pgs":
-            poker.print_all_games()
-
-
-    
-    print("\ndone")
-
 class Poker:
     def __init__(self, ledger_folder_path: str, json_path: str) -> None:
 
@@ -202,8 +166,4 @@ class Poker:
         with open(self.json_path, "w") as json_file:
             json.dump(json_data, json_file, indent=4)
                 
-
-
-if __name__ == "__main__":
-    main()
-    
+                
