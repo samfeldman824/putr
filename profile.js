c07be8adde708447259b5b4f4a28896de1389c40
samfeldman824 <samuel_feldman@brown.edu>
2023-12-11 06:55:20 -0500

    stats styling update

--- profile.js
+++ profile.js
@@ -1,7 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
         const playerName = decodeURIComponent(urlParams.get('playerName'));
         // console.log("player id is",playerID)
-
+        const createStatCard = (label, value) => `
+        <div class="stat-card">
+          <div class="stat-value">${value}</div>
+          <div class="stat-label">${label}</div>
+        </div>
+      `;
         fetch("data.json")
             .then(response => response.json())
             .then(data => {
@@ -10,24 +15,25 @@ const urlParams = new URLSearchParams(window.location.search);
             // console.log(player)
 
             if (player) {
-                const profileStats = document.getElementById("playerStats");
+                const statsContainer = document.getElementById("stats-container");
                 const nameDiv = document.getElementById("playerInfo")
                 nameDiv.innerHTML = `
               <h1>${playerName}</h1>
               `  
-                profileStats.innerHTML = `
-                        <p>PUTR: ${player.putr.toFixed(2)}</p>
-                        <p>Net: ${player.net.toFixed(2)}</p>
-                        <p>Games Played: ${player.games_played.length}</p>
-                        <p>Biggest Win: ${player.biggest_win.toFixed(2)}</p>
-                        <p>Biggest Loss: ${player.biggest_loss.toFixed(2)}</p>
-                        <p>Highest Net: ${player.highest_net.toFixed(2)}</p>
-                        <p>Lowest Net: ${player.lowest_net.toFixed(2)}</p>
-                        <p>Games Up Most: ${player.games_up_most}</p>
-                        <p>Games Down Most: ${player.games_down_most}</p>
-                        <p>Games Up: ${player.games_up}</p>
-                        <p>Games Down: ${player.games_down}</p>
-                        <p>Average Net ${player.average_net.toFixed(2)}</p>`;
+              statsContainer.innerHTML = `
+              ${createStatCard('PUTR', player.putr.toFixed(2))}
+              ${createStatCard('Net', player.net.toFixed(2))}
+              ${createStatCard('Games Played', player.games_played.length)}
+              ${createStatCard('Biggest Win', player.biggest_win.toFixed(2))}
+              ${createStatCard('Biggest Loss', player.biggest_loss.toFixed(2))}
+              ${createStatCard('Highest Net', player.highest_net.toFixed(2))}
+              ${createStatCard('Lowest Net', player.lowest_net.toFixed(2))}
+              ${createStatCard('Games Up Most', player.games_up_most)}
+              ${createStatCard('Games Down Most', player.games_down_most)}
+              ${createStatCard('Games Up', player.games_up)}
+              ${createStatCard('Games Down', player.games_down)}
+              ${createStatCard('Average Net', player.average_net.toFixed(2))}
+            `;
                         
 
                 const netDictionary = player.net_dictionary;
