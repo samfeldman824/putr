49ec9fb26eec72244cf005f285e6e88512a21c17
samfeldman824 <samuel_feldman@brown.edu>
2025-07-28 17:03:16 -0400

    moving db from local json to google firestore

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLetCCR_hcY4_2AcV21w0eYfkqhzH_viQ",
+  authDomain: "bmt-db-dc8eb.firebaseapp.com",
+  databaseURL: "https://bmt-db-dc8eb-default-rtdb.firebaseio.com",
+  projectId: "bmt-db-dc8eb",
+  storageBucket: "bmt-db-dc8eb.firebasestorage.app",
+  messagingSenderId: "417887464130",
+  appId: "1:417887464130:web:82edbc63001c4271a9e95b",
+  measurementId: "G-8L0CN18VR5"
+};
+
+
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ const db = firebase.firestore();
+
+
+
+
 let putrAsc = true;
 let netAsc  = true;
 
 
 function populateTable() {
-    fetch("data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const tableBody = document.getElementById("table-body");
-        for (let key in data) {
-          item = data[key]
-          let encodedName = encodeURIComponent(key)
-          const row = document.createElement("tr");
-          row.innerHTML = `
+  console.log("populateTable called");
+  const tableBody = document.getElementById("table-body");
+  // tableBody.innerHTML = ""; // Clear existing rows
+
+  db.collection("players").get()
+    .then((querySnapshot) => {
+      let playerCount = 0;
+      querySnapshot.forEach((doc) => {
+        const item = doc.data();
+        const key = doc.id;
+        let encodedName = encodeURIComponent(key);
+        const row = document.createElement("tr");
+        row.innerHTML = `
           <td class="flag-container">
             <img src="${item.flag}" class="player-flag"/>
           </td>
           <td class="player-name">
-      <a href="profile.html?playerName=${encodedName}"('${key}')">${key}</a>
-    </td>
+            <a href="profile.html?playerName=${encodedName}">${key}</a>
+          </td>
           <td class="player-putr">${Number.isFinite(item.putr) ? item.putr.toFixed(2) : 'UR'}</td>
           <td class="player-net">${item.net.toFixed(2)}</td>
         `;
         tableBody.appendChild(row);
-      }
-        
-        sortTableByPutr()
-
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.log("Appending row for", key, item);
+        playerCount++;
       });
-  }
+      console.log("Total rows appended:", playerCount);
+      sortTableByPutr();
+    })
+    .catch((error) => {
+      console.error("Error fetching players from Firestore:", error);
+    });
+}
+
+
 
 
 
